b869867ab6f330eecab1f425cc93c8647292ad50
Zehua Chen <31496190+zehuac2@users.noreply.github.com>
2026-08-13 23:35:16 -0700

    Setup bundle splitting.

--- astro.config.ts
+++ astro.config.ts
@@ -9,6 +9,43 @@ export default defineConfig({
   site: 'https://zehuac2.github.io',
   base: '/web-tools',
   output: 'static',
+  vite: {
+    build: {
+      chunkSizeWarningLimit: 1024,
+      rolldownOptions: {
+        output: {
+          codeSplitting: {
+            groups: [
+              {
+                name: (id) => {
+                  if (/\/node_modules\/three\//.test(id))
+                    return 'vendor-three';
+                },
+                minShareCount: 0,
+                minSize: 0,
+              },
+              {
+                name: (id) => {
+                  if (/\/node_modules\/(@react-three\/|three-stdlib\/)/.test(id))
+                    return 'vendor-r3f';
+                },
+                minShareCount: 0,
+                minSize: 0,
+              },
+              {
+                name: (id) => {
+                  if (/\/node_modules\/(react|react-dom|scheduler)\//.test(id))
+                    return 'vendor-react';
+                },
+                minShareCount: 0,
+                minSize: 0,
+              },
+            ],
+          },
+        },
+      },
+    },
+  },
   integrations: [
     react({
       babel: {
